661a67e84a469b0e94276a1c3892f78be9ca9347
yuudachi13 <t2pupa00@students.oamk.fi>
2024-10-08 14:24:54 +0300

    testif

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {firestore} from './firebase/Config';
 
 export default function App() {
   return (
