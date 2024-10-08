a374e726269a435bcaa6649fb0e475d9e49a2465
yuudachi13 <t2pupa00@students.oamk.fi>
2024-10-08 14:52:47 +0300

    toinen osa

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {firestore} from './firebase/Config';
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View,  } from 'react-native';
+import {firestore, addDoc, collection, MESSAGES, serverTimestamp} from './firebase/Config';
+import React, { useState } from 'react';
+
 
 export default function App() {
+  const [newMessage, setNewMessage] = useState('')
+
+  const save = async () => {
+    const docRef = await addDoc(collection(firestore, MESSAGES),{
+      text: newMessage,
+      created: serverTimestamp()
+    }).catch(error => console.log(error))
+
+    setNewMessage('')
+    console.log('message saved.')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.form}>
+        <TextInput 
+        placeholder='Send message...'
+        value ={newMessage}
+        onChangeText={text => setNewMessage(text)}
+        />
+        <Button title="Save" onPress={save} />
+
+      </View>
+
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +37,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'flex-start',
+    margin: 8
+  }, 
+  form:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 16,
+    marginBottom: 16,
+  }
 });
